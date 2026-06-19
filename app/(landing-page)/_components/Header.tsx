cd11b478d33e7aa2a97afb36eda2de8fbe4e6cd2
Isuu1 <jakub.babelek@gmail.com>
2026-06-19 21:10:00 +0100

    Add Footer + apply color variable in Header

--- app/(landing-page)/_components/Header.tsx
+++ app/(landing-page)/_components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center py-6 px-10 bg-gray-100">
+    <div className="flex justify-between items-center py-6 px-10 bg-card">
       <h2>Logo</h2>
       <nav className="flex gap-4">
         <Link href="/builder" className="cursor-pointer">
