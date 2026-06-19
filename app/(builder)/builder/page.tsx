52aa40b9633ee9bde69388c8f22feb7023470f75
Isuu1 <jakub.babelek@gmail.com>
2026-06-19 22:30:25 +0100

    Minor styling changes

--- app/(builder)/builder/page.tsx
+++ app/(builder)/builder/page.tsx
@@ -4,12 +4,12 @@ import PreviewPanel from "./_components/PreviewPanel";
 export default function Page() {
   return (
     <div className="w-full h-full grid-cols-2 grid gap-4 p-10">
-      <section className="flex flex-col gap-4 bg-card rounded-lg p-4">
-        <h3>Resume Builder</h3>
+      <section className="flex flex-col gap-4 bg-card rounded-2xl p-4">
+        <h4>Resume Builder</h4>
         <PersonalDetailsForm />
       </section>
-      <section className="flex flex-col gap-4 items-center">
-        <h3>Resume Preview</h3>
+      <section className="flex flex-col gap-4 bg-card rounded-2xl p-4">
+        <h4>Resume Preview</h4>
         <PreviewPanel />
       </section>
     </div>
