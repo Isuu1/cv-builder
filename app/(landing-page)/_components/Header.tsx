2a747f398b81481bee4cc1ea777630cb8743966f
Isuu1 <jakub.babelek@gmail.com>
2026-06-19 20:12:46 +0100

    Add Header and style landing page

---
+++ app/(landing-page)/_components/Header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center py-6 px-10 bg-gray-100">
+      <h2>Logo</h2>
+      <nav className="flex gap-4">
+        <Link href="/builder" className="cursor-pointer">
+          Create CV
+        </Link>
+        <span className="cursor-pointer">Browse templates</span>
+        <span className="cursor-pointer">Sign in</span>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,7 +1,9 @@
+import Header from "./_components/Header";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <div className="flex flex-col">
+      <Header />
     </div>
   );
 }
